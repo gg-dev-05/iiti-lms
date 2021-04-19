959aa63653839726eb6eee791b1065a73584853b
Anushk Gautam <me190003012@iiti.ac.in>
2021-04-19 13:54:05 +0530

    Added search functionality of books from database to dashboard

--- app.py
+++ app.py
@@ -97,13 +97,26 @@ def allBooks():
 @app.route("/book", methods=['GET', 'POST'])
 def book():
     if request.method == 'GET':
-        return "GET"
-    data = request.form
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM book WHERE title='{}'".format(data['book']))
-    books = cur.fetchall()
-    print(books)
-    return "POST"
+        return "GET";
+    if session["isAdmin"] == True:   
+        data = request.form
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM book WHERE title='{}'".format(data['book']))
+        books = cur.fetchall()
+        print(books)
+        # return render_template("searchBook.html",books=books);
+        return render_template("adminSearchBook.html",books=books);
+
+    if session["isAdmin"] == False:   
+        data = request.form
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM book WHERE title='{}'".format(data['book']))
+        books = cur.fetchall()
+        print(books)
+        # return render_template("searchBook.html",books=books);
+        return render_template("userSearchBook.html",books=books); 
+    return redirect("/");         
+
 # issue details
 @app.route("/logs")
 def logs():
