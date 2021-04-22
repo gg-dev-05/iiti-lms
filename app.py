eb74826ce0edbed5a5d23da5332f9d961257284f
gg-dev-05 <garvitgalgat@gmail.com>
2021-04-22 13:22:55 +0530

    add new book

--- app.py
+++ app.py
@@ -440,28 +440,14 @@ def addBook():
             return render_template("addBook.html", details=session["profile"])
         else:
             data = request.form
-            print(data)
-            # cur = mysql.connection.cursor()
-            # cur.execute(
-            #     f"insert into book(title,ISBN,book_language,publisher,publish_date,shelf_id) values('{data['title']}','{data['ISBN']}','{data['language']}','{data['publisher']}','{data['date']}','{data['shelf']}')")
-            # mysql.connection.commit()
-
-            # if data['tag1'] != '':
-            #     cur = mysql.connection.cursor()
-            #     cur.execute(
-            #         f"insert into tags values('{data['ISBN']}','{data['tag1']}')")
-            #     mysql.connection.commit()
-            # if data['tag2'] != '':
-            #     cur = mysql.connection.cursor()
-            #     cur.execute(
-            #         f"insert into tags values('{data['ISBN']}','{data['tag2']}')")
-            #     mysql.connection.commit()
-            # if data['tag3'] != '':
-            #     cur = mysql.connection.cursor()
-            #     cur.execute(
-            #         f"insert into tags values('{data['ISBN']}','{data['tag3']}')")
-            #     mysql.connection.commit()
-            return render_template("addBook.html", details=session["profile"])
+            cur = mysql.connection.cursor()
+            cur.execute(f"INSERT INTO book(ISBN, title, book_language, publisher, publish_date, shelf_id) VALUES({data['isbn']}, '{data['title']}', '{data['language']}', '{data['publisher']}', '{data['date']}', {data['shelf']})")
+            data = data.to_dict(flat=False)
+            for tag in data['tags']:
+                cur.execute("INSERT INTO tags VALUES ({}, '{}')".format(data['isbn'][0], tag))
+            mysql.connection.commit()
+            flash("New Book Added")
+            return redirect("/book")
     return redirect("/")
 
 @app.route("/myBooks")
